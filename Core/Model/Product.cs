11ac6d2456ba109a05262b4187d53e48950aed94
ahmed <a-nac@DESKTOP-2CC4ME5>
2021-01-27 20:29:30 +0100

    Initial commit

---
+++ Core/Model/Product.cs
@@ -0,0 +1,9 @@
+namespace Core.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Core.Model;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products {get;set;}
+    }
+}      
